0b7ca6b6f8662246c7519ec39dacc8e6b5c97dc3
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-07-29 23:16:24 -0700

    initial commit

---
+++ WeatherMapApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherMapApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.WriteLine("Please enter API key:");
+            var apiKey = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter the name of the city you would like to find:");
+                var cityName = Console.ReadLine();
+                var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={apiKey}";
+                var response = client.GetStringAsync(weatherURL).Result;
+                var formattedresponse = JObject.Parse(response).GetValue("main").ToString();
+                Console.WriteLine(formattedresponse);
+                Console.WriteLine();
+                Console.WriteLine("Would you like to choose another city?");
+                var userInput = Console.ReadLine();
+                Console.WriteLine();
+                if (userInput == "no")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
